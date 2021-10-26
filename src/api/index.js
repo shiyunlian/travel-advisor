e29f177c4ee1bf9e49092206a5fc106deef26cc7
shiyunlian <shiyunlian00@gmail.com>
2021-10-25 19:40:57 -0700

    modify api key

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,15 @@ import axios from "axios";
 // use Travel Advisor API from Rapit API website
 // https://rapidapi.com/apidojo/api/travel-advisor/
 export const getPlacesData = async (type, sw, ne) => {
-  // deconstruct response.data: {data}, since the json data body consists of data, deconstruct data again
   try {
+    // const response = await axios.get(URL, options)
+    // return response
+
+    // deconstruct response.data: {data}, since the json response data body consists of data,
+    // deconstruct data again to get the places data
+
+    // const {data:{data}} = await axios.get(URL, options)
+    // return data
     const {
       data: { data },
     } = await axios.get(
@@ -17,7 +24,7 @@ export const getPlacesData = async (type, sw, ne) => {
           tr_latitude: ne.lat, // Latitude of top right coordinate
         },
         headers: {
-          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_WEATHER_API_KEY,
+          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
           "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
         },
       }
@@ -40,7 +47,7 @@ export const getWeatherData = async (lat, lng) => {
           lat: lat,
         },
         headers: {
-          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
+          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_WEATHER_API_KEY,
           "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
         },
       }
