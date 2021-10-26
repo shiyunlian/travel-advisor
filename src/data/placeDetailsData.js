8127562872c7932876dc2a64272b2ef37fa7b96d
shiyunlian <shiyunlian00@gmail.com>
2021-10-25 18:19:55 -0700

    add placeDetails json data

---
+++ src/data/placeDetailsData.js
@@ -0,0 +1,141 @@
+// console.log(place) from PlaceDetails.js
+// {location_id: '5210035', name: 'Izakaya Ajito', latitude: '37.31255', longitude: '-122.03363', num_reviews: '10', …}
+// address: "7335 Bollinger Rd Suite C, Cupertino, CA 95014-4328"
+// address_obj: {street1: '7335 Bollinger Rd', street2: 'Suite C', city: 'Cupertino', state: 'CA', country: 'United States', …}
+// ancestors: (3) [{…}, {…}, {…}]
+// awards: []
+// bearing: "northeast"
+// category: {key: 'restaurant', name: 'Restaurant'}
+// cuisine: []
+// description: ""
+// dietary_restrictions: []
+// distance: "0.10705647006690948"
+// distance_string: "0.1 km"
+// doubleclick_zone: "na.us.california"
+// establishment_types: [{…}]
+// hours: {week_ranges: Array(7), timezone: 'America/Los_Angeles'}
+// is_candidate_for_contact_info_suppression: false
+// is_closed: false
+// is_jfy_enabled: false
+// is_long_closed: false
+// latitude: "37.31255"
+// location_id: "5210035"
+// location_string: "Cupertino, California"
+// longitude: "-122.03363"
+// name: "Izakaya Ajito"
+// nearest_metro_station: []
+// num_reviews: "10"
+// open_now_text: "Closed today"
+// parent_display_name: "Cupertino"
+// phone: "+1 408-517-8701"
+// photo:
+// caption: ""
+// helpful_votes: "0"
+// id: "367309970"
+// images:
+// large:
+// height: "550"
+// url: "https://media-cdn.tripadvisor.com/media/photo-p/15/e4/b4/92/photo3jpg.jpg"
+// width: "550"
+// [[Prototype]]: Object
+// medium: {width: '450', url: 'https://media-cdn.tripadvisor.com/media/photo-s/15/e4/b4/92/photo3jpg.jpg', height: '450'}
+// original: {width: '1280', url: 'https://media-cdn.tripadvisor.com/media/photo-m/1280/15/e4/b4/92/photo3jpg.jpg', height: '1280'}
+// small: {width: '150', url: 'https://media-cdn.tripadvisor.com/media/photo-l/15/e4/b4/92/photo3jpg.jpg', height: '150'}
+// thumbnail: {width: '50', url: 'https://media-cdn.tripadvisor.com/media/photo-t/15/e4/b4/92/photo3jpg.jpg', height: '50'}
+// [[Prototype]]: Object
+// is_blessed: true
+// published_date: "2018-12-29T23:45:22-0500"
+// uploaded_date: "2018-12-29T23:45:22-0500"
+// user: {user_id: null, member_id: '0', type: 'user'}
+// [[Prototype]]: Object
+// preferred_map_engine: "default"
+// price_level: "$$ - $$$"
+// ranking: "#91 of 213 Restaurants in Cupertino"
+// ranking_category: "restaurant"
+// ranking_denominator: "193"
+// ranking_geo: "Cupertino"
+// ranking_geo_id: "32273"
+// ranking_position: "120"
+// rating: "3.5"
+// raw_ranking: "3.051182985305786"
+// subcategory: []
+// timezone: "America/Los_Angeles"
+// web_url: "https://www.tripadvisor.com/Restaurant_Review-g32273-d5210035-Reviews-Izakaya_Ajito-Cupertino_California.html"
+// website: "http://ajito.us"
+// write_review: "https://www.tripadvisor.com/UserReview-g32273-d5210035-Izakaya_Ajito-Cupertino_California.html"
+// [[Prototype]]: Object
+
+// previous version:
+// address: "998 S de Anza Blvd, San Jose, CA 95129-2706"
+// address_obj: {street1: "998 S de Anza Blvd", street2: "", city: "San Jose", state: "CA", country: "United States", …}
+// ancestors: (3) [{…}, {…}, {…}]
+// awards: Array(1)
+// 0: {award_type: "CERTIFICATE_OF_EXCELLENCE", year: "2020", images: {…}, categories: Array(0), display_name: "Certificate of Excellence 2020"}
+// length: 1
+// [[Prototype]]: Array(0)
+// bearing: "east"
+// booking:
+// provider: "Grubhub"
+// url: "https://www.tripadvisor.com/Commerce?p=Grubhub&src=155358048&geo=360572&from=api&area=reservation_button&slot=1&matchID=1&oos=0&cnt=1&silo=25768&bucket=852508&nrank=1&crank=1&clt=R&ttype=Restaurant&tm=208545652&managed=false&capped=false&gosox=I-ADlg7wY8lqEq92hGOCPH9yxKao4fnJtoYdd5TLctoCVK2EaqcCi9rOMrUCyEbynEkE8hfYyevgs3qcBJkLatnSIm9GtestM0ilntzA_-8&cs=15ed629dfa796865cabf41e0abe109c1b"
+// [[Prototype]]: Object
+// category:
+// key: "restaurant"
+// name: "Restaurant"
+// [[Prototype]]: Object
+// cuisine: Array(3)
+// 0: {key: "9908", name: "American"}
+// 1: {key: "10665", name: "Vegetarian Friendly"}
+// 2: {key: "10992", name: "Gluten Free Options"}
+// length: 3
+// [[Prototype]]: Array(0)
+// description: "Want breakfast any time of day? Holder’s Country Inn offers delicious, tasty breakfast any time you want. We are located in San Jose near Cupertino."
+// dietary_restrictions: Array(2)
+// 0: {key: "10665", name: "Vegetarian Friendly"}
+// 1: {key: "10992", name: "Gluten Free Options"}
+// length: 2
+// [[Prototype]]: Array(0)
+// distance: "0.24976552418872106"
+// distance_string: "0.2 km"
+// doubleclick_zone: "na.us.ca.san_jose"
+// email: "deanzacountryinn@gmail.com"
+// establishment_types: Array(1)
+// 0: {key: "10591", name: "Restaurants"}
+// length: 1
+// [[Prototype]]: Array(0)
+// hours:
+// timezone: "America/Los_Angeles"
+// week_ranges: (7) [Array(1), Array(1), Array(1), Array(1), Array(1), Array(1), Array(1)]
+// [[Prototype]]: Object
+// is_candidate_for_contact_info_suppression: false
+// is_closed: false
+// is_jfy_enabled: false
+// is_long_closed: false
+// latitude: "37.31089"
+// location_id: "360572"
+// location_string: "San Jose, California"
+// longitude: "-122.03191"
+// name: "Holder's Country Inn"
+// nearest_metro_station: []
+// num_reviews: "89"
+// open_now_text: "Open Now"
+// parent_display_name: "San Jose"
+// phone: "+1 408-244-2798"
+// photo: {images: {…}, is_blessed: true, uploaded_date: "2016-02-15T17:17:50-0500", caption: "", id: "173364588", …}
+// preferred_map_engine: "default"
+// price_level: "$$ - $$$"
+// ranking: "#34 of 2,165 Restaurants in San Jose"
+// ranking_category: "restaurant"
+// ranking_denominator: "1619"
+// ranking_geo: "San Jose"
+// ranking_geo_id: "33020"
+// ranking_position: "40"
+// rating: "4.0"
+// raw_ranking: "3.6344034671783447"
+// reserve_info: {id: "360572", provider: "Grubhub", provider_img: "https://static.tacdn.com/img2/eateries/grubhub_04.23.2019.png", url: "https://www.tripadvisor.com/Commerce?p=Grubhub&src…tM0ilntzA_-8&cs=15ed629dfa796865cabf41e0abe109c1b", booking_partner_id: null, …}
+// subcategory: Array(0)
+// length: 0
+// [[Prototype]]: Array(0)
+// timezone: "America/Los_Angeles"
+// web_url: "https://www.tripadvisor.com/Restaurant_Review-g33020-d360572-Reviews-Holder_s_Country_Inn-San_Jose_California.html"
+// website: "http://holderscountryinndeanza.com"
+// write_review: "https://www.tripadvisor.com/UserR
---
+++ src/data/restaurantData.js
@@ -0,0 +1,65 @@
+// response result from restaurants/list-in-boundary api on rapid api
+
+// {6 items
+// "data":[7 items
+// 0:{46 items
+// "location_id":"11707706"
+// "name":"Kincafe - Salvaged Ring Dien Khanh"
+// "latitude":"12.264778"
+// "longitude":"109.10869"
+// "num_reviews":"1"
+// "timezone":"Asia/Ho_Chi_Minh"
+// "location_string":"Nha Trang, Khanh Hoa Province"
+// "photo":{...}8 items
+// "awards":[]0 items
+// "doubleclick_zone":"as.vietnam.khanh_hoa.nha_trang"
+// "preferred_map_engine":"default"
+// "raw_ranking":"3.027096748352051"
+// "ranking_geo":"Nha Trang"
+// "ranking_geo_id":"293928"
+// "ranking_position":"514"
+// "ranking_denominator":"634"
+// "ranking_category":"restaurant"
+// "ranking":"#396 of 633 Restaurants in Nha Trang"
+// "distance":"8.843051774354308"
+// "distance_string":"8.8 km"
+// "bearing":"south"
+// "rating":"5.0"
+// "is_closed":false
+// "open_now_text":"Open Now"
+// "is_long_closed":false
+// "price_level":"$"
+// "price":"$14,000 - $70,000"
+// "description":"Kincafe - Salvaged Ring Dien Khanh is one of the world's top 20 architecture 2014."
+// "web_url":"https://www.tripadvisor.com/Restaurant_Review-g293928-d11707706-Reviews-Kincafe_Salvaged_Ring_Dien_Khanh-Nha_Trang_Khanh_Hoa_Province.html"
+// "write_review":"https://www.tripadvisor.com/UserReview-g293928-d11707706-Kincafe_Salvaged_Ring_Dien_Khanh-Nha_Trang_Khanh_Hoa_Province.html"
+// "ancestors":[...]3 items
+// "category":{...}2 items
+// "subcategory":[...]1 item
+// "parent_display_name":"Nha Trang"
+// "is_jfy_enabled":false
+// "nearest_metro_station":[]0 items
+// "phone":"+84 89 803 40 39"
+// "website":"http://www.facebook.com/kincafenhatrang"
+// "email":"minhkhangwood@gmail.com"
+// "address_obj":{...}6 items
+// "address":"Hung Vuong Dien Khanh, Nha Trang 650000 Vietnam"
+// "hours":{...}2 items
+// "is_candidate_for_contact_info_suppression":false
+// "cuisine":[...]1 item
+// "dietary_restrictions":[]0 items
+// "establishment_types":[...]1 item
+// }
+// 1:{...}40 items
+// 2:{...}46 items
+// 3:{...}32 items
+// 4:{...}8 items
+// 5:{...}32 items
+// 6:{...}32 items
+// ]
+// "filters":{...}15 items
+// "filters_v2":{...}3 items
+// "restaurant_availability_options":{...}12 items
+// "open_hours_options":{...}8 items
+// "paging":{...}2 items
+// }
