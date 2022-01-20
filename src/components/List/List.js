55c0a12783f901c3024fd9db502c660ec005aa28
shiyunlian <shiyunlian00@gmail.com>
2022-01-20 00:53:50 -0800

    add ref={elRefs[i]} in Grid

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -64,7 +64,7 @@ const List = ({
 
           <Grid container spacing={3} className={classes.list}>
             {places?.map((place, i) => (
-              <Grid item key={i} xs={12}>
+              <Grid ref={elRefs[i]} item key={i} xs={12}>
                 <PlaceDetails
                   place={place}
                   selected={Number(childClicked) === i}
